57b01e33d4e5ed4a4cb171cb0b5e5700c5d7c481
rymcbr <ry.mcbr@gmail.com>
2017-03-25 14:04:30 -0400

    Add files via upload

--- js/builder.js
+++ js/builder.js
@@ -5,3 +5,9 @@ var joeyGrieve = joeyGrieve.replace("%date%", "March 24, 2017");
 var joeyGrieve = joeyGrieve.replace("%color%", "green");
 $("#c2").prepend(joeyGrieve);
 
+var ryanMcBride = authorCard.replace("%author%", "Ryan McBride");
+var ryanMcBride = ryanMcBride.replace("%modal%", "ryanmcbride");
+var ryanMcBride = ryanMcBride.replace("%post%", "As Andover High School approaches a new year of exciting learning and discovering new interests and ideas, a controversy arises");
+var ryanMcBride = ryanMcBride.replace("%date%", "March 25, 2017");
+var ryanMcBride = ryanMcBride.replace("%color%", "green");
+$("#c2").prepend(ryanMcBride);
