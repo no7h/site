2b6faa2d39493d362931a5efc493472e4c1976e9
joshardul <joshardul@users.noreply.github.com>
2017-03-25 17:45:29 -0400

    Add files via upload

--- js/builder.js
+++ js/builder.js
@@ -11,3 +11,120 @@ var ryanMcBride = ryanMcBride.replace("%post%", "As Andover High School approach
 var ryanMcBride = ryanMcBride.replace("%date%", "March 25, 2017");
 var ryanMcBride = ryanMcBride.replace("%color%", "green");
 $("#c2").prepend(ryanMcBride);
+
+var markHaskell = authorCard.replace("%author%", "Mark Haskell");
+var markHaskell = markHaskell.replace("%modal%", "markhaskell");
+var markHaskell = markHaskell.replace("%post%", "The new 7+H Schedule restricts the amounts of electives one can take because for students with Academic Support, it takes up 1 full credit of your classes and it means you cannot do 2 fun electives that are a 1/2 credit.");
+var markHaskell = markHaskell.replace("%date%", "March 25, 2017");
+var markHaskell = markHaskell.replace("%color%", "green");
+$("#c2").prepend(markHaskell);
+
+/* George Joseph's card goes here */
+
+var gabePapazian = authorCard.replace("%author%", "Gabe Papazian");
+var gabePapazian = gabePapazian.replace("%modal%", "gabepapazian");
+var gabePapazian = gabePapazian.replace("%post%", "I like the concept of the full year classes but I do not like how it takes away a class. I like the old one better because we had an extra class.");
+var gabePapazian = gabePapazian.replace("%date%", "March 25, 2017");
+var gabePapazian = gabePapazian.replace("%color%", "green");
+$("#c2").prepend(gabePapazian);
+
+var juliaParziale = authorCard.replace("%author%", "Julia Parziale");
+var juliaParziale = juliaParziale.replace("%modal%", "juliaparziale");
+var juliaParziale = juliaParziale.replace("%post%", "I think that it will be overwhelming for students to have 7 classes worth of homework each night even if it is not due for 2 days. It is hard to time manage 7 classes plus being involved in sports, work, etc. ");
+var juliaParziale = juliaParziale.replace("%date%", "March 25, 2017");
+var juliaParziale = juliaParziale.replace("%color%", "green");
+$("#c2").prepend(juliaParziale);
+
+/* George Truong's card goes here */
+
+var richardTang = authorCard.replace("%author%", "Richard Tang");
+var richardTang = richardTang.replace("%modal%", "richardtang");
+var richardTang = richardTang.replace("%post%", "I don't understand how it works");
+var richardTang = richardTang.replace("%date%", "March 25, 2017");
+var richardTang = richardTang.replace("%color%", "green");
+$("#c2").prepend(richardTang);
+
+var anonymous1 = authorCard.replace("%author%", "Anonymous");
+var anonymous1 = anonymous1.replace("%modal%", "anonymous1");
+var anonymous1 = anonymous1.replace("%post%", "The new scedule is a terrible decision since it limits the amount of classes students can take. As a rising junior, there are many classes than I want to take, but the new scedule doesn't allow me to do so. People are arguing that it's better to have a full year rotation and people are resistant to change, when in reality students like me just want to take more courses.");
+var anonymous1 = anonymous1.replace("%date%", "March 25, 2017");
+var anonymous1 = anonymous1.replace("%color%", "green");
+$("#c2").prepend(anonymous1);
+
+var mattTravaglini = authorCard.replace("%author%", "Matt Travaglini");
+var mattTravaglini = mattTravaglini.replace("%modal%", "matttravaglini");
+var mattTravaglini = mattTravaglini.replace("%post%", "It is too expensive and gets rid of a useful credit. Why fix something that isn't broken?");
+var mattTravaglini = mattTravaglini.replace("%date%", "March 25, 2017");
+var mattTravaglini = mattTravaglini.replace("%color%", "green");
+$("#c2").prepend(mattTravaglini);
+
+var danielBernstein = authorCard.replace("%author%", "Daniel Bernstein");
+var danielBernstein = danielBernstein.replace("%modal%", "danielbernstein");
+var danielBernstein = danielBernstein.replace("%post%", "It is too expensive and gets rid of a useful credit. Why fix something that isn't broken?");
+var danielBernstein = danielBernstein.replace("%date%", "March 25, 2017");
+var danielBernstein = danielBernstein.replace("%color%", "green");
+$("#c2").prepend(danielBernstein);
+
+var anonymous2 = authorCard.replace("%author%", "Anonymous");
+var anonymous2 = anonymous2.replace("%modal%", "anonymous2");
+var anonymous2 = anonymous2.replace("%post%", "As a student athlete I believe that the new schedule will give us to much homework and that I won't have enough time to complete it. I'm also concerned that I will not do well on tests because I won't have enough time to study due to the reason above. I also don't like how we aren't allowed to TA next year.");
+var anonymous2 = anonymous2.replace("%date%", "March 25, 2017");
+var anonymous2 = anonymous2.replace("%color%", "red");
+$("#c3").prepend(anonymous2);
+
+var anonymous3 = authorCard.replace("%author%", "Anonymous");
+var anonymous3 = anonymous3.replace("%modal%", "anonymous3");
+var anonymous3 = anonymous3.replace("%post%", "It's good that it's all year but that's about the only good thing. We can't double up on classes (example, you can't have two foreign language classes in a year as you can with the semesterized schedule) we don't have many options for our classes (7 credits instead of 8) , we'll look worse to high ranking private colleges because we haven't taken as many classes as other schools, the 8 day rotating thing is ridiculous as is H block, all of the finals will be at once, the blocks are much shorter and our tests might not be scaled down proportionately, and the teachers will not be used to giving out a proper amount of homework so they will give out far too much. Please repeal it or make a drastic change to it, my number one priority being bring back 8 credits or at least 7.5");
+var anonymous3 = anonymous3.replace("%date%", "March 25, 2017");
+var anonymous3 = anonymous3.replace("%color%", "green");
+$("#c2").prepend(anonymous3);
+
+var anonymous4 = authorCard.replace("%author%", "Anonymous");
+var anonymous4 = anonymous4.replace("%modal%", "anonymous4");
+var anonymous4 = anonymous4.replace("%post%", "I think the new schedule is awful. no one wants it so why are even doing it?? also now we take one less class because we have one less half credit. we already miss credit opportunities by being allowed to only take AP'a as a junior. also, having AP bio and chem as 1.5 credits is obscene.. and  the idea of having the same subject at different times of the day is ridiculous. isn't change supposed to be good???? way 2 go AHS");
+var anonymous4 = anonymous4.replace("%date%", "March 25, 2017");
+var anonymous4 = anonymous4.replace("%color%", "green");
+$("#c2").prepend(anonymous4);
+
+var anonymous5 = authorCard.replace("%author%", "Anonymous");
+var anonymous5 = anonymous5.replace("%modal%", "anonymous5");
+var anonymous5 = anonymous5.replace("%post%", "I don't think that this new schedule will benifit  the students education  since we will be losing the option to select classes for a whole credit. Many of us who are going to be juniors in the fall are looking to take AP classes to help get into a better college and to be able to take courses we are interested in. Without the option to take 8 credits it makes it extremely hard to fit in the AP courses we want to take and limits the amount of interesting electives we can take. I have been looking forward to selecting cool electives since junior year offers more options but now that I have to fill my schedule with core classes and AP there is no room for those courses where as if the old schedule was still her I could take two electives of choice. I think that this schedule does not take into account the students profile and interests and places more limitations than opportunities.");
+var anonymous5 = anonymous5.replace("%date%", "March 25, 2017");
+var anonymous5 = anonymous5.replace("%color%", "green");
+$("#c2").prepend(anonymous5);
+
+/* Abhay's card goes here */
+
+var tylerBeraldi = authorCard.replace("%author%", "Tyler Beraldi");
+var tylerBeraldi = tylerBeraldi.replace("%modal%", "tylerberaldi");
+var tylerBeraldi = tylerBeraldi.replace("%post%", "Unnecessary and tedious. Schedule now is fine, but if there has to be a change, this should not be the schedule implemented.");
+var tylerBeraldi = tylerBeraldi.replace("%date%", "March 25, 2017");
+var tylerBeraldi = tylerBeraldi.replace("%color%", "green");
+$("#c2").prepend(tylerBeraldi);
+
+/* Manas' card goes here */
+
+var arshTiku = authorCard.replace("%author%", "Arsh Tiku");
+var arshTiku = arshTiku.replace("%modal%", "arshtiku");
+var arshTiku = arshTiku.replace("%post%", "Do it after next year.");
+var arshTiku = arshTiku.replace("%date%", "March 25, 2017");
+var arshTiku = arshTiku.replace("%color%", "red");
+$("#c3").prepend(arshTiku);
+
+var anonymous6 = authorCard.replace("%author%", "Anonymous");
+var anonymous6 = anonymous6.replace("%modal%", "anonymous6");
+var anonymous6 = anonymous6.replace("%post%", "In my opinion, the 7+H schedule is not viable. The school lacks the budget and the students lack the preparation to go into the next year with this schedule. It will revert students to a middle school-style schedule rather than one that they will experience in college, a semester system with four classes per semester much like our current one. Additionally, it prevents students from doubling up on classes to get to the highest level of education possible in high school. This will leave students with a lesser degree of knowledge and make them less competitive to colleges. Overall, the 7+H schedule would be a huge step backwards in the attempt to advance Andover High School.");
+var anonymous6 = anonymous6.replace("%date%", "March 25, 2017");
+var anonymous6 = anonymous6.replace("%color%", "red");
+$("#c3").prepend(anonymous6);
+
+/* Rahul's card goes here */
+
+var anonymous7 = authorCard.replace("%author%", "Anonymous");
+var anonymous7 = anonymous7.replace("%modal%", "anonymous7");
+var anonymous7 = anonymous7.replace("%post%", "It really limits the opportunities that students have to take AP and extracurricular classes because of how it only allows 7 credits, which could dictate which colleges they get into. There is no way for me to take as many APs or as many extracurricular classes as I would like to because of how restricting it is.");
+var anonymous7 = anonymous7.replace("%date%", "March 25, 2017");
+var anonymous7 = anonymous7.replace("%color%", "green");
+$("#c2").prepend(anonymous7);
+
+/* Ishaan's card goes here */
