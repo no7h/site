1abbe46d2c8017a544569e958413dc70140494f7
joshardul <joshardul@users.noreply.github.com>
2017-03-24 23:11:28 -0400

    Update init.js

--- js/init.js
+++ js/init.js
@@ -4,3 +4,21 @@
       closeOnClick: false, // Closes side-nav on <a> clicks, useful for Angular/Meteor
       draggable: true // Choose whether you can drag to open on touch screens
   });
+$(document).ready(function(){
+    $('.modal').modal();
+    $('.parallax').parallax();
+    $('.carousel').carousel();
+    $(".button-collapse").sideNav();
+    $('.scrollspy').scrollSpy();
+    $('.materialboxed').materialbox();
+    $('.slider').slider({full_width: true});
+    $('.collapsible').collapsible({accordion : false});
+    $('ul.tabs').tabs();
+    $('.datepicker').pickadate({
+        selectMonths: true,
+        selectYears: 15,
+    });
+    $(document).ready(function() {
+    $('select').material_select();
+    });
+});
