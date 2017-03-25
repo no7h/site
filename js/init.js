c7274306eb75d7193a810597b2b47ac386dd63b3
rymcbr <ry.mcbr@gmail.com>
2017-03-25 14:18:15 -0400

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,9 +1,3 @@
-  $('.button-collapse').sideNav({
-      menuWidth: 70, // Default is 300
-      edge: 'left', // Choose the horizontal origin
-      closeOnClick: false, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-      draggable: true // Choose whether you can drag to open on touch screens
-  });
 $(document).ready(function(){
     $('.modal').modal();
     $('.parallax').parallax();
@@ -18,7 +12,12 @@ $(document).ready(function(){
         selectMonths: true,
         selectYears: 15,
     });
-    $(document).ready(function() {
     $('select').material_select();
-    });
+     $('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 300
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true // Choose whether you can drag to open on touch screens
+    }
+  );
 });
